fa0cdd749702fe925ff53481330a6ffb472a61a1
Satya Parimi <parimisatya30@gmail.com>
2025-11-15 17:13:42 +0800

    Add OrderService.Domain with Order entity

---
+++ MyHomeTest/Src/OrderService/OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public Order(Guid userId, string product, int quantity, decimal price)
+        {
+            UserId = userId;
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
