e9c40310eff8fe3d5b3f8b7fbfd3f892fa1852c8
Satya Parimi <parimisatya30@gmail.com>
2025-11-15 17:16:05 +0800

    Add remaining solution files and cleanup untracked items

---
+++ MyHomeTest/Src/Shared/Events/OrderCreatedEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Events
+{
+    public class OrderCreatedEvent
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public OrderCreatedEvent(Guid id, Guid userId, string product, int quantity, decimal price)
+        {
+            Id = id;
+            UserId = userId;
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
---
+++ MyHomeTest/Src/Shared/Events/UserCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Events
+{
+    public class UserCreatedEvent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public UserCreatedEvent(Guid id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+    }
+}
