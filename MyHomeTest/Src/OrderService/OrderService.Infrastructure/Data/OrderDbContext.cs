17693c6f91300967600486daa4c5b67db3fa1e7f
Satya Parimi <parimisatya30@gmail.com>
2025-11-15 17:14:25 +0800

    Add OrderService.Infrastructure with DbContext and Kafka producer

---
+++ MyHomeTest/Src/OrderService/OrderService.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+using System.Collections.Generic;
+
+namespace OrderService.Infrastructure.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+    }
+}
---
+++ MyHomeTest/Src/OrderService/OrderService.Infrastructure/Kafka/KafkaProducer.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.Kafka;
+
+namespace OrderService.Infrastructure.Kafka
+{
+    public class KafkaProducer
+    {
+        private readonly IProducer<string, string> _producer;
+
+        public KafkaProducer(string bootstrapServers)
+        {
+            var config = new ProducerConfig { BootstrapServers = bootstrapServers };
+            _producer = new ProducerBuilder<string, string>(config).Build();
+        }
+
+        public async Task ProduceAsync(string topic, string message)
+        {
+            await _producer.ProduceAsync(topic, new Message<string, string> { Key = Guid.NewGuid().ToString(), Value = message });
+        }
+    }
+}
