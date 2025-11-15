321047f78a369fd5ea1e534fa44b312f5003fd26
Satya Parimi <parimisatya30@gmail.com>
2025-11-15 17:12:00 +0800

    Add UserService.Domain with User entity

---
+++ MyHomeTest/Src/UserService/UserService.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace UserService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
