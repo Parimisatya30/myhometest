f115d7feb6af877af554d67e785382a639568a96
Satya Parimi <parimisatya30@gmail.com>
2025-11-15 17:12:40 +0800

    Add UserService.Infrastructure with DbContext and Kafka producer

---
+++ MyHomeTest/Src/UserService/UserService.Infrastructure/Data/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using UserService.Domain.Entities;
+
+namespace UserService.Infrastructure.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+    }
+}
